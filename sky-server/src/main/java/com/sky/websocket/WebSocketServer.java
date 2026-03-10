52e48871d430e6a13c9d693fea55a233bd97fa57
13924 <13924910142@163.com>
2026-03-10 16:22:04 +0800

    面试专用

---
+++ sky-server/src/main/java/com/sky/websocket/WebSocketServer.java
@@ -0,0 +1,86 @@
+package com.sky.websocket;
+
+import org.springframework.stereotype.Component;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.websocket.*;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * WebSocket服务
+ */
+@Component
+@ServerEndpoint("/ws/{sid}")
+@Slf4j
+public class WebSocketServer {
+
+    // 存放会话对象
+    private static Map<String, Session> sessionMap = new HashMap<>();
+
+    /**
+     * 连接建立成功调用的方法
+     */
+    @OnOpen
+    public void onOpen(Session session, @PathParam("sid") String sid) {
+        log.info("客户端：{} 建立连接", sid);
+        sessionMap.put(sid, session);
+    }
+
+    /**
+     * 收到客户端消息后调用的方法
+     *
+     * @param message 客户端发送过来的消息
+     */
+    @OnMessage
+    public void onMessage(String message, @PathParam("sid") String sid) {
+        log.info("收到来自客户端：{} 的信息：{}", sid, message);
+    }
+
+    /**
+     * 连接关闭调用的方法
+     *
+     * @param sid
+     */
+    @OnClose
+    public void onClose(@PathParam("sid") String sid) {
+        log.info("连接断开：{}", sid);
+        sessionMap.remove(sid);
+    }
+
+    /**
+     * 群发消息
+     *
+     * @param message
+     */
+    public void sendToAllClient(String message) {
+        Collection<Session> sessions = sessionMap.values();
+        for (Session session : sessions) {
+            try {
+                session.getBasicRemote().sendText(message);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 单点发送消息
+     *
+     * @param sid
+     * @param message
+     */
+    public void sendToClient(String sid, String message) {
+        Session session = sessionMap.get(sid);
+        if (session != null) {
+            try {
+                session.getBasicRemote().sendText(message);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
